fafffaa90f1e8c4a58e45e399575178c8ed4df3c
23fajar23 <fajardarussalam20@gmail.com>
2024-04-16 13:51:01 +0700

    Complete Sort Character in program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SortCharacter sortCharacter = new SortCharacter();
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine(" 1. Sort Character ");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~");
+            sortCharacter.run();
         }
     }
 }
---
+++ ConsoleApp1/SortCharacter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class SortCharacter
+    {
+        public string[] vowels = ["a", "i", "u", "e", "o"];
+        public string procVowel(string param)
+        {
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < param.Length; i++)
+            {
+                foreach (string vowel in vowels)
+                {
+                    if (param.Substring(i, 1) == vowel)
+                    {
+                        list.Add(vowel);
+                    }
+                }
+            }
+
+            if (list.Count == 0)
+            {
+                return "No Vowel";
+            }
+
+            return string.Join("", sortData(list));
+        }
+
+        public string procConsonant(string param)
+        {
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < param.Length; i++)
+            {
+                Boolean available = false;
+                foreach (string vowel in vowels)
+                {
+                    if (param.Substring(i, 1) == vowel)
+                    {
+                        available = true;
+                    }
+                }
+
+                if (!available)
+                {
+                    list.Add(param.Substring(i, 1));
+                }
+
+            }
+
+            if (list.Count == 0)
+            {
+                return "No Consonant";
+            }
+
+            return string.Join("", sortData(list));
+        }
+
+        public List<string> sortData(List<string> input)
+        {
+            List<string> distinctData = input.Distinct().ToList();
+            List<string> sortedData = new List<string>();
+
+            for (int i = 0; i < distinctData.Count; i++)
+            {
+                foreach (var character in input)
+                {
+                    if (distinctData.ElementAt(i) == character)
+                    {
+                        sortedData.Add(character);
+                    }
+                }
+            }
+
+            return sortedData;
+        }
+
+        public void run()
+        {
+            Console.Write("Input one line of words (S) : ");
+            try
+            {
+                string input = Console.ReadLine().Replace(" ", "").ToLower();
+
+                string charVowel = procVowel(input);
+                string charConsonant = procConsonant(input);
+
+                Console.WriteLine("Vowel characters : ");
+                Console.WriteLine(charVowel);
+                Console.WriteLine("Consonant Characters : ");
+                Console.WriteLine(charConsonant);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
+        }
+    }
+}
